4155e8b941d2046081bf19ee0dfcf7430f2042a4
Grzegorz-Pacia <grzegorz.pacia@student.wsb.edu.pl>
2026-04-20 15:06:39 +0200

    Usunięto zbędny kod z metody SetTileContent i zmieniono jej typ zwracany na void, poprawiono błędy oraz formatowanie i dodano komentarze.

--- Zadanie2C#/Program.cs
+++ Zadanie2C#/Program.cs
@@ -8,10 +8,10 @@ namespace Zadanie2C_
 {
     class Player
     {
-        private string playerName { get; set; }
-        private char playerSymbol { get; set; }
+        private string playerName { get; set; } //nazwa gracza
+        private char playerSymbol { get; set; } //symbol gracza (X lub O)
 
-        public Player(string playerName, char playerSymbol)
+        public Player(string playerName, char playerSymbol) //ustawia nazwę i symbol gracza
         {
             this.playerName = playerName;
             this.playerSymbol = playerSymbol;
@@ -24,9 +24,9 @@ namespace Zadanie2C_
 
     class Board
     {
-        private char[,] boardContents;
+        private char[,] boardContents; //tablica, której elementy stanowią pola planszy
 
-        public Board()
+        public Board() //tworzy tablicę 3x3 reprezentującą planszę i wypełnia ją spacjami
         {
             this.boardContents = new char[3, 3];
             for (int i = 0; i < 3; i++)
@@ -38,47 +38,39 @@ namespace Zadanie2C_
             }
         }
 
-        public char GetTileContent(int x, int y)
+        public char GetTileContent(int x, int y) //zwraca zawartość danego pola
         {
             return boardContents[x, y];
         }
 
-        public bool SetTileContent(int x, int y, char symbol)
+        public void SetTileContent(int x, int y, char symbol) //ustawia podany symbol jako zawartość danego pola
         {
-            if (boardContents[x, y] == 'X' || boardContents[x, y] == 'O')
-            {
-                return false;
-            }
-            else
-            {
                 boardContents[x, y] = symbol;
-                return true;
-            }
         }
 
-        public char CheckWinCondition()
+        public char CheckWinCondition() //sprawdza, czy któryś gracz zwyciężył, doszło do remisu lub można dalej gracz
         {
-            char[] possibleWinner = { ' ', ' ', ' ' };
+            char[] possibleWinner = { ' ', ' ', ' ' }; //przechowuje wartości pól [1, 1], [0, 0] oraz [2, 2] jeśli zostanie wykryte zwyciężające ustawienie znaków. Zwycięstwo zostanie wykryte również, gdy będą to 3 spacje, więc są to tylko potencjalne zwycięstwa.
 
-            if ((boardContents[0, 0] == boardContents[1, 1] && boardContents[1, 1] == boardContents[2, 2]) ||
+            if ((boardContents[0, 0] == boardContents[1, 1] && boardContents[1, 1] == boardContents[2, 2]) || //wykrywa zwycięstwa zawierające pole [1, 1]
                 (boardContents[0, 2] == boardContents[1, 1] && boardContents[1, 1] == boardContents[2, 0]) ||
                 (boardContents[0, 1] == boardContents[1, 1] && boardContents[1, 1] == boardContents[2, 1]) ||
                 (boardContents[1, 0] == boardContents[1, 1] && boardContents[1, 1] == boardContents[1, 2]))
             {
                 possibleWinner[0] = boardContents[1, 1];
             }
-            else if ((boardContents[0, 0] == boardContents[0, 1] && boardContents[0, 1] == boardContents[0, 2]) ||
+            else if ((boardContents[0, 0] == boardContents[0, 1] && boardContents[0, 1] == boardContents[0, 2]) || //wykrywa zwycięstwa zawierające pole [0, 0]
                 (boardContents[0, 0] == boardContents[1, 0] && boardContents[1, 0] == boardContents[2, 0]))
             {
                 possibleWinner[1] = boardContents[0, 0];
             }
-            else if ((boardContents[2, 2] == boardContents[2, 1] && boardContents[2, 1] == boardContents[2, 0]) ||
+            else if ((boardContents[2, 2] == boardContents[2, 1] && boardContents[2, 1] == boardContents[2, 0]) || //wykrywa zwycięstwa zawierające pole [2, 2]
                 (boardContents[2, 2] == boardContents[1, 2] && boardContents[1, 2] == boardContents[0, 2]))
             {
                 possibleWinner[2] = boardContents[2, 2];
             }
 
-            foreach (char symbol in possibleWinner)
+            foreach (char symbol in possibleWinner) //jeśli któreś potencjalne zwycięstwo nie jest spacją - zwraca odpowiadający mu znak
             {
                 if (symbol != ' ')
                 {
@@ -86,7 +78,7 @@ namespace Zadanie2C_
                 }
             }
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++) //wyszukuje spacje w tablicy. Jeśli jakąś znajdzie, zwraca spację, co oznacza, że można dalej grać.
             {
                 for (int j = 0; j < 3; j++)
                 {
@@ -97,10 +89,10 @@ namespace Zadanie2C_
                 }
             }
 
-            return 'T';
+            return 'T'; //jeśli żaden ze wcześniejszych warunków nie został spełniony, zwraca T sygnalizujące remis
         }
 
-        public void DisplayBoardContents()
+        public void DisplayBoardContents() //wypisuje w konsoli zawartość tablicy wraz z liniami oddzielającymi pola
         {
             for (int i = 0; i < 3; i++)
             {
@@ -123,11 +115,11 @@ namespace Zadanie2C_
 
     class Game
     {
-        private Board board;
-        private Player[] players;
-        private int currentPlayer;
+        private Board board; //przechowuje planszę do danej gry
+        private Player[] players; //przechowuje obu graczy danej gry
+        private int currentPlayer; //który gracz w tablicy będzie wykonywał następną turę
 
-        public Game(string player1Name, string player2Name)
+        public Game(string player1Name, string player2Name) //tworzy planszę, tablicę graczy i umieszcza w niej dwóch graczy o podanych nazwach i odpowiednich symbolach, ustawia pierwszego z nich jako wykonującego następną turę
         {
             this.board = new Board();
             this.players = new Player[2];
@@ -136,14 +128,14 @@ namespace Zadanie2C_
             this.currentPlayer = 0;
         }
 
-        public bool nextTurn()
+        public bool NextTurn() //przeprowadza kolejną turę gry
         {
             Console.Clear();
-            Console.WriteLine("Tura gracza " + players[currentPlayer].getPlayerName() + ".");
+            Console.WriteLine("Tura gracza " + players[currentPlayer].getPlayerName() + ".\n");
             board.DisplayBoardContents();
-            Console.WriteLine("Podaj pole, na którym chcesz umieścić symbol \"" + players[currentPlayer].getPlayerSymbol() + "\" w postaci \"x y\",\n" +
+            Console.WriteLine("\nPodaj pole, na którym chcesz umieścić symbol \"" + players[currentPlayer].getPlayerSymbol() + "\", w postaci \"x y\",\n" +
                               "gdzie x to numer wiersza, a y to numer kolumny:");
-            do
+            do //powtarza się do momentu, gdy obecny gracz w poprawny sposób poda niezajęte pole
             {
                 string targetTile = Console.ReadLine();
                 if (targetTile.Length == 3 && targetTile[1] == ' '
@@ -151,7 +143,7 @@ namespace Zadanie2C_
                    && (targetTile[2] == '1' || targetTile[2] == '2' || targetTile[2] == '3')
                    && (board.GetTileContent((targetTile[0]-'1'), (targetTile[2]-'1')) == ' '))
                 {
-                    board.SetTileContent((targetTile[0]-'1'), (targetTile[2]-'1'), players[currentPlayer].getPlayerSymbol());
+                    board.SetTileContent((targetTile[0]-'1'), (targetTile[2]-'1'), players[currentPlayer].getPlayerSymbol()); //odjęcie '1' od wartości char daje tu poprawną wartość int pola w tablicy (indexowanie od 0)
                     break;
                 }
                 else
@@ -161,22 +153,22 @@ namespace Zadanie2C_
                 }
             } while(true);
 
-            if (board.CheckWinCondition() == players[currentPlayer].getPlayerSymbol())
+            if (board.CheckWinCondition() == players[currentPlayer].getPlayerSymbol()) //sprawdza, czy obecny gracz wygrał i kończy grę
             {
                 Console.Clear();
                 board.DisplayBoardContents();
-                Console.WriteLine("Zwyciężą gracz " + players[currentPlayer].getPlayerName() + "!");
-                return false;
+                Console.WriteLine("\nZwycięża gracz " + players[currentPlayer].getPlayerName() + "!");
+                return false; //gra się zakończyła
             }
-            else if (board.CheckWinCondition() == 'T')
+            else if (board.CheckWinCondition() == 'T') //sprawdza, czy doszło do remisu i kończy grę
             {
                 Console.Clear();
                 board.DisplayBoardContents();
-                Console.WriteLine("Remis!");
-                return false;
+                Console.WriteLine("\nRemis!");
+                return false; //gra się zakończyła
             }
-            currentPlayer = Math.Abs(currentPlayer - 1);
-            return true;
+            currentPlayer = Math.Abs(currentPlayer - 1); //zamienia obecnego gracza
+            return true; //gra jest kontynuowana
         }
     }
 
@@ -184,8 +176,8 @@ namespace Zadanie2C_
     {
         static void Main(string[] args)
         {
-            Game game1 = new Game("Gracz 1", "Gracz 2");
-            while(game1.nextTurn());
+            Game game1 = new Game("Gracz 1", "Gracz 2"); //tworzy obiekt gry, którego konstruktor tworzy dwa obiekty graczy (z podanymi nazwami) i obiekt planszy
+            while(game1.NextTurn()); //powtarza się do momentu, gdy metoda NextTurn zwróci false, czyli do zakończenia się gry
         }
     }
 }
